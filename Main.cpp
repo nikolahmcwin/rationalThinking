6d379748c3c37a0375f1406fd735c8ff150a846e
Finlay Luxton <luxfi448@student.otago.ac.nz>
2018-05-06 12:17:55 +1200

    Moved Rational.* to temporary folder to avoid compiling errors

--- Main.cpp
+++ Main.cpp
@@ -11,25 +11,25 @@
 int main() {
 
     e9::Integer i1(); 
-	e9::Integer i2("123456789");
+	//e9::Integer i2("123456789");
 
     // Prove it was input correctly
-	std::cout << "This program demonstrates Integer numbers." << std::endl;
-    std::cout << "If i1 = " << i1 << " and i2 = " << i2 << " then:" << std::endl;
+	//std::cout << "This program demonstrates Integer numbers." << std::endl;
+    //std::cout << "If i1 = " << i1 << " and i2 = " << i2 << " then:" << std::endl;
     
     // Addition
-	e9::Integer sum = i1 + i2;
-    std::cout << "The sum i1 + i2 = " << sum << std::endl;
+	//e9::Integer sum = i1 + i2;
+    //std::cout << "The sum i1 + i2 = " << sum << std::endl;
     
     //Multiplication
-	std::cout << "The product i1 * i2 = " << i1 * i2 << std::endl;
+	//std::cout << "The product i1 * i2 = " << i1 * i2 << std::endl;
 
-	std::cout << "Please enter new values for i1 and i2." << std::endl;
-	std::cin >> i1 >> i2;
-    std::cout << "Now, i1 = " << i1 << " and i2 = " << i2 << std::endl;
+	//std::cout << "Please enter new values for i1 and i2." << std::endl;
+	//std::cin >> i1 >> i2;
+    //std::cout << "Now, i1 = " << i1 << " and i2 = " << i2 << std::endl;
 
-	std::cout << "New, i1 + i2 = " << i1 + i2 << std::endl;
-	std::cout << "New, i1 * i2 = " << i1 * i2 << std::endl;
+	//std::cout << "New, i1 + i2 = " << i1 + i2 << std::endl;
+	//std::cout << "New, i1 * i2 = " << i1 * i2 << std::endl;
 
     return 0;
 }
